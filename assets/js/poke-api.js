23899df77effc12ce333c2a25643d24d5ec1a56b
juliogfk <juliogfk1@gmail.com>
2022-11-30 09:22:26 -0300

    Pokedex ajustada com hover e organização do poke-api. Acredito que seja a vewrsão final.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -6,17 +6,17 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    
+    pokemon.types = types
+    pokemon.type = type
+    
     const abilities = pokeDetail.abilities.map((abilitiesSlot) => abilitiesSlot.ability.name)
     const [ability] = abilities
 
     pokemon.abilities = abilities
     pokemon.ability = ability
-
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
-
-    pokemon.types = types
-    pokemon.type = type
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
